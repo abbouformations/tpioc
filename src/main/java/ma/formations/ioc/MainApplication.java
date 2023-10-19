0b874869065a7457444f196624c80fb015257ef9
abbouformations <135717843+abbouformations@users.noreply.github.com>
2023-10-19 14:25:59 +0100

    Update MainApplication.java

--- src/main/java/ma/formations/ioc/MainApplication.java
+++ src/main/java/ma/formations/ioc/MainApplication.java
@@ -1,19 +1,15 @@
 package ma.formations.ioc;
 
-import ma.formations.ioc.dao.DaoImpl1;
-import ma.formations.ioc.dao.IDao;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "ma.formations.ioc")
 public class MainApplication {
-    // Injection par factory
-    @Bean
+
+/*    @Bean
     @Qualifier("dao1")
     public IDao getDao() {
         return new DaoImpl1();
-    }
+    }*/
 }
